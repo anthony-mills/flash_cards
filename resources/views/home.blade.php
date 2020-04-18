3cf4edd59f6bf17ca3b3fa35ea2041d25d6eb9ea
Anthony Mills <anthony.mills@tmsw.com>
2020-04-19 09:47:31 +1200

    Added card management options to the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Flash Cards</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,12 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                        
+                    <ul>
+                        <li>Create Card</li>                        
+                        <li>List Cards</li>
+                        <li>Manage Categories</li>
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,14 @@
     <div id="app">
         <nav  class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                        Home
-                </a>
+                @guest
+
+                @else
+                    <a class="navbar-brand" href="{{ url('/home') }}">
+                            Home
+                    </a>
+                @endif
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
