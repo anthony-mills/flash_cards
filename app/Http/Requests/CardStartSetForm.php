4c1226e795f56c3841e454177edd087098229754
Anthony Mills <anthony.mills@tmsw.com>
2020-04-20 20:12:27 +1200

    Created start page allowing selelction of topic and the max number of cards requested

---
+++ app/Http/Requests/CardStartSetForm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class CardStartSetForm extends FormRequest
+{
+    protected $_reqObj;
+
+    /**
+     * @param  \Illuminate\Http\Request $reqObj
+     */
+    public function __construct(Request $reqObj)
+    {
+        $this->_reqObj = $reqObj;
+
+        parent::__construct();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'category' => 'required|numeric',
+            'card_number' => 'required|numeric',                    
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator( $validator )
+    {
+        $validator->after(function ($validator) {
+            $formErrors = $validator->errors();
+            $errorMsg = '';
+
+            foreach ($formErrors->all() as $errMsg) {
+                $errorMsg .= '<li>' . $errMsg . '</li>';
+            }
+
+            if (strlen($errorMsg) > 0) {
+                $this->_reqObj->session()->put(
+                    'error', 
+                    '<ul>' . $errorMsg . '</ul>'
+                ); 
+            }           
+        });
+    }    
+}
---
+++ resources/views/cards/start.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Get Started</div>
+
+                <div class="card-body">
+                    @include('alerts.status')
+
+                    @if ( $cardCats->count() > 0 )
+                        {!! Form::open(['class' => 'form', 'route' => 'card.begin']) !!}
+
+                            <!-- Card Category -->
+                            <div class="form-group">
+                                {!! Form::label('category', 'Topic') !!}
+                                <span class="red-text">*</span>
+                                <br />
+
+                                {!! Form::select( 'category', $cardCats->pluck('name', 'id'), null, ['dusk' => 'card_category', 'required'] ) !!}
+                            </div>
+
+                            <!-- Number of cards -->
+                            <div class="form-group">
+                                {!! Form::label('card_number', 'Card Number') !!}
+                                <span class="red-text">*</span>
+                                <br />
+
+                                {!! Form::select( 'card_number', $cardNumber, null, ['dusk' => 'card_number', 'required'] ) !!}
+                            </div>
+
+                            <div class="text-center">
+                                {!! Form::submit('Continue', ['class' => 'btn btn-primary', 'dusk' => 'continue_button']) !!}
+                            </div>                      
+                        {!! Form::close() !!}
+                    @else 
+                        <p>Some topics and cards need to be added to the system before you can proceed.</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
