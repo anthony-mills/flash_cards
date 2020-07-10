db994e24b62ef2ca520d079561227929dcea9cdf
Anthony Mills <anthony.mills@tmsw.com>
2020-07-10 22:11:07 +1200

    Added browser test case to test card category creation

---
+++ tests/Browser/Tests/Categories/CreateCategoryTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace tests\Browser\Tests\Categories;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Config;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Tests\Browser\Pages\AuthSession;
+
+use App\Models\CardCategories;
+
+use App\User;
+
+
+class CreateCategoryTest extends DuskTestCase
+{
+    // Category Name 
+    protected $categoryName = 'Test Category';
+
+    // URL for card creation
+    protected $cardCreateURL = '/card/categories/create';
+
+    // User account to run tests under
+    protected $testUser = 1;
+
+    
+    public function testCreation()
+    {
+        echo "\r\nBrowser Tests: Running category creation tests. \r\n";
+
+        $this->createCategoryFail();
+        $this->createCategorySuccess();
+
+        CardCategories::where('name', $this->categoryName)->delete();
+    }
+
+    /**
+    * Test the creation of a new category with an invalid category name
+    *
+    * @return void
+    **/ 
+    protected function createCategoryFail()
+    {
+        $this->browse(function (Browser $browserObj) {
+            $browserObj->loginAs(User::find(1))
+                    ->visit( Config::get('app.url') . $this->cardCreateURL )
+                    ->assertVisible('@name')
+                    ->assertVisible('@save_category')
+                    ->type('name', '!INVALID_CATEGORY$$$NAME')
+                    ->click('@save_category')
+                    ->waitFor('.alert')
+                    ->assertVisible('.alert-danger')                    
+                    ->assertSeeIn('.alert-danger', 'The name format is invalid.');
+        });                   
+    }
+
+    /**
+    * Test the successful creation of a new category
+    *
+    * @return void
+    **/ 
+    protected function createCategorySuccess()
+    {
+        $this->browse(function (Browser $browserObj) {
+            $browserObj->loginAs(User::find(1))
+                    ->visit( Config::get('app.url') . $this->cardCreateURL )
+                    ->assertVisible('@name')
+                    ->assertVisible('@save_category')
+                    ->type('name', $this->categoryName)
+                    ->click('@save_category')
+                    ->waitFor('.alert')
+                    ->assertVisible('.alert-success')                    
+                    ->assertSeeIn('.alert-success', 'Saved the new category "' . $this->categoryName . '".');
+        });                   
+    }
+}
---
+++ tests/DuskTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\TestCase as BaseTestCase;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected $headlessTest = true;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        static::startChromeDriver();
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $chromeArgs = array(
+            '--window-size=1920,1080'
+        );
+        
+        if ( $this->headlessTest ) {
+            array_push( $chromeArgs, '--disable-gpu', '--headless' );        
+        }
+
+        $options = (new ChromeOptions)->addArguments( $chromeArgs ); 
+
+        return RemoteWebDriver::create(
+            'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+        );
+    }
+}
