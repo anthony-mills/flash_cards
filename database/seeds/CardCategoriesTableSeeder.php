36bad6ac629326a84a3db568f38256ad88bc47f1
Anthony Mills <anthony.mills@tmsw.com>
2020-07-13 19:47:43 +1200

    Fixed typo in the card migration definition, updated the card seed file

--- database/seeds/CardCategoriesTableSeeder.php
+++ database/seeds/CardCategoriesTableSeeder.php
@@ -19,31 +19,73 @@ class CardCategoriesTableSeeder extends Seeder
         \DB::table('card_categories')->insert(array (
             0 => 
             array (
+                'created_at' => '2020-04-19 11:10:08',
                 'id' => 1,
                 'name' => 'AWS Basics',
-                'created_at' => '2020-04-19 23:10:08',
-                'updated_at' => '2020-04-19 23:10:08',
+                'updated_at' => '2020-04-19 11:10:08',
             ),
             1 => 
             array (
+                'created_at' => '2020-04-21 06:14:55',
                 'id' => 2,
                 'name' => 'AWS EC2',
-                'created_at' => '2020-04-21 06:14:55',
                 'updated_at' => '2020-04-21 06:14:55',
             ),
             2 => 
             array (
+                'created_at' => '2020-04-21 06:17:17',
                 'id' => 3,
                 'name' => 'AWS S3',
-                'created_at' => '2020-04-21 06:17:17',
                 'updated_at' => '2020-04-21 06:17:17',
             ),
             3 => 
             array (
+                'created_at' => '2020-04-21 08:31:10',
                 'id' => 4,
                 'name' => 'AWS WAF',
-                'created_at' => '2020-04-21 08:31:10',
                 'updated_at' => '2020-04-21 08:31:10',
+            ),
+            4 => 
+            array (
+                'created_at' => '2020-04-23 05:32:21',
+                'id' => 5,
+                'name' => 'AWS Databases',
+                'updated_at' => '2020-04-23 05:32:21',
+            ),
+            5 => 
+            array (
+                'created_at' => '2020-04-24 06:44:50',
+                'id' => 6,
+                'name' => 'AWS Elasticache',
+                'updated_at' => '2020-04-24 06:44:50',
+            ),
+            6 => 
+            array (
+                'created_at' => '2020-04-24 07:02:54',
+                'id' => 7,
+                'name' => 'AWS DNS',
+                'updated_at' => '2020-04-24 07:02:54',
+            ),
+            7 => 
+            array (
+                'created_at' => '2020-04-25 08:23:22',
+                'id' => 9,
+                'name' => 'AWS Networking',
+                'updated_at' => '2020-04-25 08:23:22',
+            ),
+            8 => 
+            array (
+                'created_at' => '2020-05-09 22:15:58',
+                'id' => 10,
+                'name' => 'AWS Security',
+                'updated_at' => '2020-05-09 22:15:58',
+            ),
+            9 => 
+            array (
+                'created_at' => '2020-06-19 23:09:22',
+                'id' => 11,
+                'name' => 'AWS CloudFormation',
+                'updated_at' => '2020-06-19 23:09:22',
             ),
         ));
         
