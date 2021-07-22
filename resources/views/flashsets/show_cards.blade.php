afe3de275609cbeb0b550efd553a9024f59d115c
Anthony Mills <github@anthony-mills.com>
2021-07-22 18:09:45 +1000

    Added model for leaving feedback about a card / question.

--- resources/views/flashsets/show_cards.blade.php
+++ resources/views/flashsets/show_cards.blade.php
@@ -39,14 +39,63 @@
 									<div class="current-card-side">
 										Side: <span id="current-side">Question</span>
 									</div>	
-								</div>												
+								</div>
+								
+								<div class="feedback-icon">
+									<a href="#" data-toggle="modal" data-target="#feedback-modal" title="Leave Card Feedback">
+										<img src="/img/feedback.png"/>
+									</a>
+								</div>
 							</div>
 
 							<div class="col-xs-1 text-left">
 								<span class="icon" id="next" data-icon=">"><span class="visuallyhidden">Next</span></span>									
 							</div> 																
 						</div>						
-					</div>				
+					</div>
+					<div class="modal fade" id="feedback-modal" tabindex="-1" role="dialog" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title">Leave Card Feedback</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<p class="text-center">
+										Do you like this card? Hate it? Do you believe it's incorrect?
+									</p>
+
+									<p class="text-center">
+										Leave your feedback below:
+									</p>
+
+									<form id="card-feedback">
+										{!! Form::token() !!}
+
+
+										<div class="form-group">
+										{!! Form::textarea(
+											'feedback',  
+											null, 
+											[
+												'dusk' => 'card_feedback',
+												'class' => 'form-control', 
+												'placeholder' => 'Message ( Max 250 Characters )', 
+												'maxlength' => 250 
+											])
+										!!}
+										</div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-primary">Send</button>									
+							</div>
+						</div>
+					</div>
+					</div>									
 				@else 
 					@include('alerts.insufficient_cards')
 				@endif					
